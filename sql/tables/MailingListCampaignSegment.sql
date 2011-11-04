60881ef4ca48c0a3dec0eac3dd7ed8a9aca37836
Isaac Grant <isaac@silverorange.com>
2011-11-04 10:16:10 +0000

    add a shortname for analytics use, and rename rules to the more accurate segment_options
    git-svn-id: file:///Users/gauthierm/svnrepo/public/deliverance/trunk@66433 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/MailingListCampaignSegment.sql
+++ sql/tables/MailingListCampaignSegment.sql
@@ -1,9 +1,13 @@
 create table MailingListCampaignSegment (
 	id serial,
 
+	shortname varchar(255) not null,
 	title varchar(255) not null,
 	displayorder integer not null default 0,
-	rules text,
+	segment_options text,
 
 	primary key (id)
 );
+
+create index MailingListCampaignSegment_shortname_index
+	on MailingListCampaignSegment(shortname);
