20f1ff9902e46456bb031c409d60434fadce5ce5
Isaac Grant <isaac@silverorange.com>
2011-11-07 19:58:50 +0000

    big updates to Deliverance's MailChimp support.  - drops XML/RPC backend for MailChimp's wrapper, which is REST based. This is because Pear's XML/RPC2 library wasn't closing its curl connection properly. This was a large contributor to the 10 concurrent connections exception we'd been seeing more and more of.  - Because the wrapper is 1.3 based, update to the new API. refs #7704  - as we no longer depend on curl and XML/RPC we don't have as many exception types to juggle. Add DeliveranceExceptions instead of using SiteException.  - better queuing support for batchSubscribe() and batchUnsubscribe() when MailChimp goes down or times out for some or all chunks of the update.  - remove some config definitions that are no longer necessary.
    This still needs some testing, but should all work.
    
    
    git-svn-id: file:///Users/gauthierm/svnrepo/public/deliverance/trunk@66508 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Deliverance/exceptions/DeliveranceCampaignException.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'Swat/exceptions/SwatException.php';
+
+/**
+ * Exception caused by Deliverance Campaign API calls
+ *
+ * Example exception causes are API timeouts, etc.
+ *
+ * @package   Deliverance
+ * @copyright 2011 silverorange
+ */
+class DeliveranceCampaignException extends SwatException
+{
+}
+
+?>
