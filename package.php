770eecd7684d4ed764f0bc3670ed8e9a4041b3a8
Keith Burgoyne <keith@silverorange.com>
2013-07-24 16:25:26 -0300

    prepare for release of 0.2.28 | package party

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '0.2.27';
+$version = '0.2.28';
 $notes = <<<EOT
 No release notes for you!
 EOT;
@@ -16,7 +16,7 @@ PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
 $result = $package->setOptions(
 	array(
-		'filelistgenerator' => 'svn',
+		'filelistgenerator' => 'file',
 		'simpleoutput'      => true,
 		'baseinstalldir'    => '/',
 		'packagedirectory'  => './',
