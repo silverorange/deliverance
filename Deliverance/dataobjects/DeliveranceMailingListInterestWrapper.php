6215096e126c8e6ef92b3925214aec01c05c5468
Charles Waddell <charles@silverorange.com>
2014-04-15 16:14:29 -0300

    Add default flag and dataobjects.

---
+++ Deliverance/dataobjects/DeliveranceMailingListInterestWrapper.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once 'SwatDB/SwatDBRecordsetWrapper.php';
+require_once 'Deliverance/dataobjects/DeliveranceMailingListInterest.php';
+
+/**
+ * A recordset wrapper class for DeliveranceMailingListInterest objects
+ *
+ * @package   Deliverance
+ * @copyright 2014 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       DeliveranceMailingListInterest
+ */
+class DeliveranceMailingListInterestWrapper extends SwatDBRecordsetWrapper
+{
+	// {{{ public function getShortnames()
+
+	public function getShortnames()
+	{
+		$shortnames = array();
+
+		foreach ($this as $interest) {
+			$shortnames[] = $interest->shortname;
+		}
+
+		return $shortnames;
+	}
+
+	// }}}
+	// {{{ public function getDefaultShortnames()
+
+	public function getDefaultShortnames()
+	{
+		$shortnames = array();
+
+		foreach ($this as $interest) {
+			if ($interest->is_default) {
+				$shortnames[] = $interest->shortname;
+			}
+		}
+
+		return $shortnames;
+	}
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+
+		$this->row_wrapper_class = SwatDBClassMap::get(
+			'DeliveranceMailingListInerest'
+		);
+
+		$this->index_field = 'id';
+	}
+
+	// }}}
+}
+
+?>
